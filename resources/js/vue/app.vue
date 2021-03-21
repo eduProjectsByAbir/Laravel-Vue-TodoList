4488ad8c365def2d6e0107c9afedb3841baf2de4
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2021-03-21 16:34:48 +0600

    store todo

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,7 +1,37 @@
 <template>
-    <div>Hello</div>
+    <div class="todoListContainer">
+        <div class="heading">
+            <h2 id="title">Todo List</h2>
+            <add-item-form />
+        </div>
+        <list-view />
+    </div>
 </template>
 
 <script>
-export default {};
+import addItemForm from "./addItemForm";
+import listView from "./listView";
+
+export default {
+    components: {
+        addItemForm,
+        listView
+    }
+};
 </script>
+
+<style scoped>
+.todoListContainer {
+    width: 300px;
+    margin: auto;
+}
+
+.heading {
+    background: #6e6e6e;
+    padding: 10px;
+}
+
+#title {
+    text-align: center;
+}
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>View</div>
+</template>
+
+<script>
+export default {};
+</script>
